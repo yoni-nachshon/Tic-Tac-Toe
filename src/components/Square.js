9162fa1f3f6f2c10e03049f1534e3070ac9eeb46
yoni-nice <Yoni.Nacson@nice.com>
2022-05-22 14:08:45 +0300

    change the color of the board

--- src/components/Square.js
+++ src/components/Square.js
@@ -10,7 +10,7 @@ const style = (winLine) => ({
 	fontSize: "30px",
 	cursor: "pointer",
 	border: winLine ? "2px solid #03a9f4" : "none",
-	color: winLine ? "#FFFFFF" : "#9E9E9E",
-	background: winLine ? "#03a9f4" : ""
+	color: "#FFFFFF",
+	background: winLine ? "#03a9f4" : "#A9A9A9"
 });
 export default Square;
